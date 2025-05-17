c9f32c91965bdc1dc2d08678d5d4beede8bbf7cf
rezaysya <152801523+jadden11@users.noreply.github.com>
2025-05-17 19:56:40 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const Header = () => {
   return (
     <div className="relative py-10 overflow-hidden rounded-3xl">
-      <div className="absolute inset-0 -z-10 bg-[url('/reza.png')] bg-cover bg-center blur-sm" />
+      <div className="absolute inset-0 -z-10 bg-[url('/reza.png')] bg-cover bg-center blur-xs" />
       <div className="container mx-auto px-5 font-[family-name:var(--font-poppins)] relative z-10 ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-36 items-center">
           {/* About me */}
           <div className="mr-10 sm:mt-10 fontfamily-name:var(--font-poppins)">
-            <h1 className="text-xl font-semibold text-gray-600 dark:text-gray-400 mb-2 sm:text-3xl md:">
+            <h1 className="text-xl font-semibold text-gray-900 dark:text-gray-800 mb-2 sm:text-3xl md:">
               Software Engineer
             </h1>
-            <h1 className="text-4xl font-bold text-blue-200 dark:text-blue-200 mb-3 sm:text-5xl">
+            <h1 className="text-4xl font-bold text-blue-700 dark:text-blue-700 mb-3 sm:text-5xl">
               Mohammad Reza
             </h1>
-            <p className="text-lg text-black dark:text-gray-300 leading-relaxed mb-5 sm:text-2xl">
+            <p className="text-lg text-black dark:text-gray-500 leading-relaxed mb-5 sm:text-2xl">
               Experienced in building scalable applications and passionate about
               problem-solving with clean, efficient code. Continuously learning
               and seeking growth opportunities.
